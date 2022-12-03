8eb4dfaa6cd404122a86c5fff2dcd64f77625e3d
asierrodri <81629553+asierrodri@users.noreply.github.com>
2022-12-03 20:07:41 +0100

    l

---
+++ libft/libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asirodri <asirodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 18:10:49 by asirodri          #+#    #+#             */
+/*   Updated: 2022/12/03 19:58:17 by asirodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <inttypes.h>
+# include <unistd.h>
+# include <strings.h>
+# include <limits.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+
+#endif
