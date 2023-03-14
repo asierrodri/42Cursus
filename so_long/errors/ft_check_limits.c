d35bffd83d2eaf867a95dd55be0e3840d783148b
asierrodri <81629553+asierrodri@users.noreply.github.com>
2023-03-14 19:41:13 +0100

    l

--- so_long/errors/ft_check_limits.c
+++ so_long/errors/ft_check_limits.c
@@ -3,14 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_limits.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: asirodri <asirodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 20:27:00 by asirodri          #+#    #+#             */
-/*   Updated: 2023/03/04 18:43:44 by marvin           ###   ########.fr       */
+/*   Updated: 2023/03/14 19:38:27 by asirodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+int	ft_check_1(t_game *game, int x, int y)
+{
+	while (x > 0)
+	{
+		if (game->map[y - 1][x - 1] != '1')
+			return (1);
+		x--;
+	}
+	while (y > 0)
+	{
+		if (game->map[y - 1][x] != '1')
+			return (1);
+		y--;
+	}
+	return (0);
+}
 
 int	ft_check_limits(t_game *game)
 {
@@ -31,17 +48,5 @@ int	ft_check_limits(t_game *game)
 			return (1);
 		y++;
 	}
-	while (x > 0)
-	{
-		if (game->map[y - 1][x - 1] != '1')
-			return (1);
-		x--;
-	}
-	while (y > 0)
-	{
-		if (game->map[y - 1][x] != '1')
-			return (1);
-		y--;
-	}
-	return (0);
+	return (ft_check_1(game, x, y));
 }
